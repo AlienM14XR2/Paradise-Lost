be5bcf6b14a687c606da36c12c6e04f977dab969
Jack <alienm14xr2@gmail.com>
2024-03-09 16:16:20 +0900

    REST API - FastCGI。 - エンドポイント開発。 - fcgi の簡易的な仕組みを実装した。

--- REST-API/src/main.cpp
+++ REST-API/src/main.cpp
@@ -4,14 +4,31 @@
  * fcgi を用いたエンドポイントに該当する。
  * 
  * e.g. compile.
- * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror -I../inc/ main.cpp -o ../bin/api
+ * g++ -O3 -DDEBUG -std=c++20 -pedantic-errors -Wall -Werror -I../inc/ main.cpp -lfcgi++ -lfcgi -o ../bin/api
 */
 #include <string>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcgi_config.h>
+#include <fcgi_stdio.h>
 
 
 int main(void) {
     puts("=== START REST API");
+    int count = 0;      // これが答えだったか、何らかのオブジェクトを Pool するならここで行い
+    while(FCGI_Accept() >= 0)
+    {
+        printf("content-type:text/html\r\n");
+        printf("\r\n");
+        printf("<title>Fast CGI Hello</title>");
+        printf("<h1>fast CGI hello</h1>");
+
+        printf("request uri is %s\n", getenv("REQUEST_URI"));   // これをもとに各処理に分岐できる、REST API のエンドポイントとして充分使えそう。
+        printf("Request number %d running on host<i>%s</i>\n",++count,getenv("SERVER_NAME"));
+    }
+    // while を抜けた際に、取得したメモリは解放する
     puts("END   REST API ===");
     return 0;
 }
