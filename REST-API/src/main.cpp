e417d8ced817a28d81a0cfbdf696c62b1fdbbc0e
Jack <alienm14xr2@gmail.com>
2024-03-10 21:06:59 +0900

    REST API - FastCGI。 - エンドポイント。 - POST されたデータ（文字列）の取得サンプルを実装した。

--- REST-API/src/main.cpp
+++ REST-API/src/main.cpp
@@ -21,23 +21,46 @@
 #include <fcgi_config.h>
 #include <fcgi_stdio.h>
 
+#include "rest_api_debug.hpp"
 
 int main(void) {
     std::cout << "START REST API" << std::endl;
-    // puts("=== START REST API");
-    int count = 0;      // これが答えだったか、何らかのオブジェクトを Pool するならここで行い
-    while(FCGI_Accept() >= 0)
-    {
-        printf("content-type:text/html\r\n");
-        printf("\r\n");
-        printf("<title>Fast CGI Hello</title>");
-        printf("<h1>fast CGI hello</h1>");
+    try {
+        int count = 0;      // これが答えだったか、何らかのオブジェクトを Pool するならここで行い
+        while(FCGI_Accept() >= 0)
+        {
+            printf("content-type:text/html\r\n");
+            printf("\r\n");
+            printf("<title>Fast CGI Hello</title>");
+            printf("<h1>fast CGI hello</h1>");
 
-        printf("request uri is %s\n", getenv("REQUEST_URI"));   // これをもとに各処理に分岐できる、REST API のエンドポイントとして充分使えそう。
-        printf("Request number %d running on host<i>%s</i>\n",++count,getenv("SERVER_NAME"));
+            char* contentLength = nullptr;
+            contentLength = getenv("CONTENT_LENGTH");
+            if(contentLength) {
+                ptr_api_debug<const char*, const char*>("CONTENT_LENGTH: ", contentLength);
+                auto size = atoll(contentLength);
+                char* buf = nullptr;
+                buf = (char*)malloc(size + 1);
+                if(buf) {
+                    for(auto i=0; i < size+1; i++) {
+                        buf[i] = '\0';
+                    }
+                    auto r_size = FCGI_fread(buf, size, 1, FCGI_stdin);
+                    ptr_api_debug<const char*, const decltype(r_size)&>("r_size: ", r_size);
+                    ptr_api_debug<const char*, const char*>("buf: ", buf);
+                    free(buf);
+                }
+            }
+
+            printf("request uri is %s\n", getenv("REQUEST_URI"));   // これをもとに各処理に分岐できる、REST API のエンドポイントとして充分使えそう。
+            printf("Request number %d running on host<i>%s</i>\n",++count,getenv("SERVER_NAME"));
+        }
+        // while を抜けた際に、取得したメモリは解放する
+        puts("END   REST API ===");
+        std::cout << "END REST API" << std::endl;
+        return EXIT_SUCCESS;
+    } catch(std::exception& e) {
+        ptr_api_error<const decltype(e)&>(e);
+        return EXIT_FAILURE;
     }
-    // while を抜けた際に、取得したメモリは解放する
-    puts("END   REST API ===");
-    std::cout << "END REST API" << std::endl;
-    return 0;
 }
